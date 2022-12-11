c743e449a3bd93adaf1be3760538b3c974429bc0
Karam19 <karam.shbeb1@hotmail.com>
2022-12-11 19:01:08 +0300

    add smart contract

---
+++ contracts/Patents.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Patents is ERC721URIStorage {
+    // Counter to track the tokenID
+    using Counters for Counters.Counter;
+    Counters.Counter public _tokenIds;
+
+    // Owner of the contract
+    address owner;
+
+    /*
+    Mapping from the patentID to it's status
+    0 ====> Pending
+    1 ====> Accepted
+    2 ====> Rejected
+    */
+    mapping(uint => uint) public status;
+    /*
+    Mapping from addresses to their permissions
+    true ===> Reviewer
+    false ===> Isn't a reviewer
+    */
+    mapping(address => bool) public isReviewer;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender not authorized");
+        _;
+    }
+    modifier onlyReviewer() {
+        require(isReviewer[msg.sender], "Sender is not a reviewer");
+        _;
+    }
+
+    constructor() ERC721("PatentItem", "PTM") {
+        owner = msg.sender;
+        isReviewer[owner] = true;
+    }
+
+    function submitPatent(string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        status[newItemId] = 0;
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+
+    function grantReviewPermission(address newReviewer) public onlyOwner {
+        isReviewer[newReviewer] = true;
+    }
+
+    function reviewPatent(uint256 patentId, bool decision) public onlyReviewer {
+        if (decision == true) {
+            status[patentId] = 1;
+        } else {
+            status[patentId] = 2;
+        }
+    }
+}
